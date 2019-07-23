c92a5241591e390888e5d149d6512a67cfeaa48a
cob16 <public+github@cormacbrady.info>
2019-07-23 17:22:59 +0100

    block bucket public acl

--- state.tf
+++ state.tf
@@ -39,3 +39,12 @@ resource "aws_s3_bucket" "terraform-state-storage-s3" {
     }
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "private" {
+  bucket = aws_s3_bucket.terraform-state-storage-s3.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
